fdea065234f21262b778ca9eb7acd20aaef0f733
zhangfengxue <1669610782@qq.com>
2020-11-29 17:04:41 +0800

    更新

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Hello from './components/Hello'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>你好,react脚手架</h1>
+        <Hello/>
+      </div>
+    )
+  }
+}
---
+++ src/components/Hello/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './index.css'
+export default class Hello extends Component {
+    render() {
+        return (
+            <div>
+                <h2 className = 'title'>脚手架........</h2>
+            </div>
+        )
+    }
+}
+
